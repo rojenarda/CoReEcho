2d9131cbaee6612a152a25b1840637f3379b9ed6
Rojen Arda Şeşen <sesen19@itu.edu.tr>
2024-12-11 14:24:32 +0300

    Remove regressor bias

--- coreecho/regressor.py
+++ coreecho/regressor.py
@@ -10,6 +10,5 @@ def get_shallow_mlp_head(dim_in, dim_out=1):
             torch.nn.Dropout(0.4),
             torch.nn.Linear(dim_in, dim_out),
         )
-    regressor[-1].bias.data[0] = 55.6
     
     return regressor
